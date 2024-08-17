9d0d7a52199ea51e6d2bd7e9f310be46a4a88b43
Jujusempix <julio.guilherme@grupointegrado.br>
2024-08-16 21:23:50 -0300

    vrum vrum v0.2

--- index.java
+++ index.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class index {
     public static void main(String[] args) {
+        Scanner prompt = new Scanner(System.in);
+
+
         System.out.println("Hello World");
+        int horas = 5, minutos = 30, segundos = 35, tempo = 60;
+
+        horas = (horas * tempo) * tempo;
+        minutos = minutos * tempo;
+        segundos = segundos + minutos + horas;
+
+        System.out.println("Resultado: " + segundos);
+
+
+        System.out.println("informer hora:");
+        int horas = prompt.nextInt();
+        System.out.println("informer minutos:");
+        int minutos = prompt.nextInt();
+        System.out.println("informer segundos:");
+        int segundos = prompt.nextInt();
+
     }
 }
