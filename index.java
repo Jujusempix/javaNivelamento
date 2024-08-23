7859dc5ce04e2ad9dcb9f8e64892e98c00ce8478
Jujusempix <julio.guilherme@grupointegrado.br>
2024-08-23 20:59:13 -0300

    vrum vrum V0.4

--- index.java
+++ index.java
@@ -3,18 +3,41 @@ import java.util.Scanner;
 public class index {
     public static void main(String[] args) {
         Scanner prompt = new Scanner(System.in);
+        Integer maiorValor, maiorPosicao;
 
-        System.out.println("Informe um número:");
-        Integer numero1 = prompt.nextInt();
-        System.out.println("Informe um número:");
-        Integer numero2 = prompt.nextInt();
+        System.out.println("Informe primeiro número:");
+        Integer n1 = prompt.nextInt();
+        maiorValor = n1;
+        maiorPosicao = 1;
 
-        if(numero1 > numero2){
-            System.out.println("numero 1 maior que numero 2");
-        } else if(numero2 > numero1){
-            System.out.println("numero 2 maior que numero 1");
-        } else {
-            System.out.println("tudo igual no bagui");
+        System.out.println("Informe segundo número:");
+        Integer n2 = prompt.nextInt();
+        if (n2 > maiorValor){
+            maiorValor = n2;
+            maiorPosicao = 2;
         }
+
+        System.out.println("Informe terceiro número:");
+        Integer n3 = prompt.nextInt();
+        if (n3 > maiorValor){
+            maiorValor = n3;
+            maiorPosicao = 3;
+        }
+
+        System.out.println("Informe quarto número:");
+        Integer n4 = prompt.nextInt();
+        if (n4 > maiorValor){
+            maiorValor = n4;
+            maiorPosicao = 4;
+        }
+
+        System.out.println("Informe quinto número:");
+        Integer n5 = prompt.nextInt();
+        if (n5 > maiorValor){
+            maiorValor = n5;
+            maiorPosicao = 5;
+        }
+
+        System.out.println("o mais pica é "+maiorValor+" na posição "+maiorPosicao);
     }
 }
