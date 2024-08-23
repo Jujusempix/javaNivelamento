30dac74d23349146138078ea7b0cf28b2ddd8f48
Jujusempix <julio.guilherme@grupointegrado.br>
2024-08-23 20:39:02 -0300

    vrum vrum V0.3

--- index.java
+++ index.java
@@ -4,23 +4,17 @@ public class index {
     public static void main(String[] args) {
         Scanner prompt = new Scanner(System.in);
 
+        System.out.println("Informe um número:");
+        Integer numero1 = prompt.nextInt();
+        System.out.println("Informe um número:");
+        Integer numero2 = prompt.nextInt();
 
-        System.out.println("Hello World");
-        int horas = 5, minutos = 30, segundos = 35, tempo = 60;
-
-        horas = (horas * tempo) * tempo;
-        minutos = minutos * tempo;
-        segundos = segundos + minutos + horas;
-
-        System.out.println("Resultado: " + segundos);
-
-
-        System.out.println("informer hora:");
-        int horas = prompt.nextInt();
-        System.out.println("informer minutos:");
-        int minutos = prompt.nextInt();
-        System.out.println("informer segundos:");
-        int segundos = prompt.nextInt();
-
+        if(numero1 > numero2){
+            System.out.println("numero 1 maior que numero 2");
+        } else if(numero2 > numero1){
+            System.out.println("numero 2 maior que numero 1");
+        } else {
+            System.out.println("tudo igual no bagui");
+        }
     }
 }
