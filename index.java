9f98a5ff68541a9a764abbfadda15714fcf74fa0
Jujusempix <julio.guilherme@grupointegrado.br>
2024-08-23 22:04:16 -0300

    vrum vrum V0.5

--- index.java
+++ index.java
@@ -3,41 +3,22 @@ import java.util.Scanner;
 public class index {
     public static void main(String[] args) {
         Scanner prompt = new Scanner(System.in);
-        Integer maiorValor, maiorPosicao;
+        final Integer senhaCorreta = 12345 , quantidadeMaxima=3;
+        Integer senhaDigitada = 0, quantidadeTentativa=0;
 
-        System.out.println("Informe primeiro número:");
-        Integer n1 = prompt.nextInt();
-        maiorValor = n1;
-        maiorPosicao = 1;
+        do {
+            System.out.println("Informe a senha");
+            senhaDigitada = prompt.nextInt();
+            if (!senhaDigitada.equals(senhaCorreta)){
+                System.out.println("Senha Errada");
+                quantidadeTentativa++;
+            }
+        }while(!senhaDigitada.equals(senhaCorreta) && quantidadeTentativa < quantidadeMaxima);
 
-        System.out.println("Informe segundo número:");
-        Integer n2 = prompt.nextInt();
-        if (n2 > maiorValor){
-            maiorValor = n2;
-            maiorPosicao = 2;
+        if(quantidadeTentativa.equals(quantidadeMaxima)){
+            System.out.println("BANIDO");
+        }else{
+            System.out.println("Senha Correta");
         }
-
-        System.out.println("Informe terceiro número:");
-        Integer n3 = prompt.nextInt();
-        if (n3 > maiorValor){
-            maiorValor = n3;
-            maiorPosicao = 3;
-        }
-
-        System.out.println("Informe quarto número:");
-        Integer n4 = prompt.nextInt();
-        if (n4 > maiorValor){
-            maiorValor = n4;
-            maiorPosicao = 4;
-        }
-
-        System.out.println("Informe quinto número:");
-        Integer n5 = prompt.nextInt();
-        if (n5 > maiorValor){
-            maiorValor = n5;
-            maiorPosicao = 5;
-        }
-
-        System.out.println("o mais pica é "+maiorValor+" na posição "+maiorPosicao);
     }
 }
